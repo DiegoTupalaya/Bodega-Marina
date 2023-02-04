bc0bd992bfc48234744507c2e4088c88be413604
DiegoTupalaya <dtupa19@gmail.com>
2023-02-04 00:15:54 -0500

    listar

---
+++ ControllerClientes.js
@@ -0,0 +1,34 @@
+import { getClientes } from "./firebase.js";
+
+let tabla = document.getElementById('listaclientes')
+window.addEventListener('DOMContentLoaded', async()=>{
+
+    const querySnapchot = await getClientes();
+
+    let html = "";
+    let i = 0
+    querySnapchot.forEach(doc => {
+            let clientes = doc.data();
+            
+            i++
+            html += `
+            
+                <tr>
+                    <th scope="row">${i}</th>
+                    <td>${clientes.nombre}</td>
+                    <td>${clientes.telefono}</td>
+                    <td>${clientes.deuda}</td>
+                    <td>
+                        <button>Agregar Monto</button>
+                        <button>Cancelar Monto</button>
+                    </td>
+                </tr>
+            `;
+
+            tabla.innerHTML = html;
+    });
+
+    /*querySnapchot.forEach(element => {
+        
+    });*/
+})
---
+++ firebase.js
@@ -0,0 +1,28 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+  import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCYCuwMp0ORJdYtK_p_KGw95mu-EL60mk0",
+    authDomain: "bodega-marina-c7aac.firebaseapp.com",
+    projectId: "bodega-marina-c7aac",
+    storageBucket: "bodega-marina-c7aac.appspot.com",
+    messagingSenderId: "400009372410",
+    appId: "1:400009372410:web:b06dadef25675c310b9f43",
+    measurementId: "G-9J1XPLNEKS"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const db = getFirestore(app);
+  export const getClientes = () => getDocs(collection(db, 'Clientes'));
+
